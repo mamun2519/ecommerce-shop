4e497397634afe0e9490ae6399b67f7a907ca4c0
Md Mamun <juborajvai22@gmail.com>
2022-08-21 13:02:46 +0600

    product section complect

--- src/components/page/Home/Products.js
+++ src/components/page/Home/Products.js
@@ -1,13 +1,15 @@
 import React from "react";
-
+import { FcLike } from 'react-icons/fc';
+import { GrView } from 'react-icons/gr';
+import { AiFillStar} from 'react-icons/ai';
 const Products = () => {
   return (
     <div className="max-w-7xl m-auto px-2 my-20">
       <h1 className="text-xl mb-5">Top Categories Products</h1>
       <div className="grid grid-cols-4 ">
         <div className="w-60  ">
-          <div class="card  bg-base-100 border rounded-lg h-80">
-            <div class="card-bod p-2">
+          <div className="card bg-base-100 border rounded-lg h-80">
+            <div className="card-bod p-2">
               <div className="border-b px-4 ">
                 <p className="py-3">Home</p>
               </div>
@@ -31,62 +33,104 @@ const Products = () => {
         </div>
         <div className="w-full col-span-3">
           <div className=" grid grid-cols-3 gap-5">
-            <div class="card h-96 bg-base-200 shadow-xl relative ">
-              <figure class="px-5 pt-5">
+            <div className="card  h-[410px]  bg-base-100 shadow-xl relative ">
+              <figure className="px-5 pt-5">
                 <img
-                  src="https://placeimg.com/400/225/arch"
+                  src="https://www.graphixking.com/wp-content/uploads/2020/08/Software-Engineer-Black-Cotton-T-shirt.jpg"
                   alt="Shoes"
-                  class="rounded-xl"
+                  className="rounded-xl h-[175px] w-full"
                 />
               </figure>
-              <div class="card-b px-5 pt-3">
-              <p class="badge badge-secondary p-2 absolute top-[25px] right-[2px]  rotate-45">In Stcok</p>
-                <h2 class="card-title pb-0">Smart T-Shart  </h2>
+              <div className="card-b  px-5 pt-3">
+              <p className="badge badge-secondary p-2 absolute top-[25px] right-[2px]  rotate-45">In Stcok</p>
+                <h2 className="card-title pb-0">Smart T-Shart  </h2>
                
-                <span>Man Fashion</span>
-                <p className="text-xl text-red-400">999$</p>
+                <span className=" text-gray-500">Man Fashion</span>
+                <p className="text-xl text-red-400 font-medium">999$</p>
                 <small className="line-through">1200$</small>
                 
+                <div className="flex mt-2  text-orange-400 gap-1">
+                  <span><AiFillStar/></span>
+                  <span><AiFillStar/></span>
+                  <span><AiFillStar/></span>
+                  <span><AiFillStar/></span>
+                  <span><AiFillStar/></span>
+                </div>
                 
-                
-                <div>
-                <button class=" px-4 py-2 bg-red-400 rounded-lg text-white">Buy Now</button>
+                <div className="mt-5 flex gap-3">
+                <button className=" px-4 w-1/2 py-2 bg-red-400 rounded-lg text-white ">Buy Now</button>
+                <button className=" px-4 py-2 text-xl bg-base-200 rounded-lg text-white"><span className=""><FcLike/></span></button>
+                <button className=" px-4 py-2 text-xl bg-base-200 text-red-400 rounded-lg"><span className=""><GrView/></span></button>
                 </div>
               </div>
               
             </div>
-            <div class="card bg-base-100 shadow-xl">
-              <figure class="px-10 pt-10">
+            <div className="card h-[410px] bg-base-100 shadow-xl relative ">
+              <figure className="px-5 pt-5">
                 <img
-                  src="https://placeimg.com/400/225/arch"
+                  src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/151642341/original/ec88e1330b7577bb4b39c4f50fb05eedfc3ca2fb/create-an-awesome-tshirt-design-for-you.jpg"
                   alt="Shoes"
-                  class="rounded-xl"
+                  className="rounded-xl h-[175px] w-full"
                 />
               </figure>
-              <div class="card-body items-center text-center">
-                <h2 class="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions">
-                  <button class="btn btn-primary">Buy Now</button>
+              <div className="card-b  px-5 pt-3">
+              <p className="badge badge-secondary p-2 absolute top-[25px] right-[2px]  rotate-45">In Stcok</p>
+                <h2 className="card-title pb-0">Smart T-Shart  </h2>
+               
+                <span className=" text-gray-500">Man Fashion</span>
+                <p className="text-xl text-red-400 font-medium">999$</p>
+                <small className="line-through">1200$</small>
+                
+                <div className="flex mt-2  text-orange-400 gap-1">
+                  <span><AiFillStar/></span>
+                  <span><AiFillStar/></span>
+                  <span><AiFillStar/></span>
+                  <span><AiFillStar/></span>
+                  <span><AiFillStar/></span>
+                </div>
+                
+                <div className="mt-5 flex gap-3">
+                <button className=" px-4 w-1/2 py-2 bg-red-400 rounded-lg text-white ">Buy Now</button>
+                <button className=" px-4 py-2 text-xl bg-base-200 rounded-lg text-white"><span className=""><FcLike/></span></button>
+                <button className=" px-4 py-2 text-xl bg-base-200 text-red-400 rounded-lg"><span className=""><GrView/></span></button>
                 </div>
               </div>
+              
             </div>
-            <div class="card bg-base-100 shadow-xl">
-              <figure class="px-10 pt-10">
+            <div className="card h-[410px] bg-base-100 shadow-xl relative ">
+              <figure className="px-5 pt-5">
                 <img
-                  src="https://placeimg.com/400/225/arch"
+                  src="https://static-01.daraz.com.bd/p/0dcdbc879f87b8c7f66c10393b7a5788.jpg"
                   alt="Shoes"
-                  class="rounded-xl"
+                  
+                  className="rounded-xl h-[175px] w-full"
                 />
               </figure>
-              <div class="card-body items-center text-center">
-                <h2 class="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions">
-                  <button class="btn btn-primary">Buy Now</button>
+              <div className="card-b  px-5 pt-3">
+              <p className="badge badge-secondary p-2 absolute top-[25px] right-[2px]  rotate-45">In Stcok</p>
+                <h2 className="card-title pb-0">Smart T-Shart  </h2>
+               
+                <span className=" text-gray-500">Man Fashion</span>
+                <p className="text-xl text-red-400 font-medium">999$</p>
+                <small className="line-through">1200$</small>
+                
+                <div className="flex mt-2  text-orange-400 gap-1">
+                  <span><AiFillStar/></span>
+                  <span><AiFillStar/></span>
+                  <span><AiFillStar/></span>
+                  <span><AiFillStar/></span>
+                  <span><AiFillStar/></span>
+                </div>
+                
+                <div className="mt-5 flex gap-3">
+                <button className=" px-4 w-1/2 py-2 bg-red-400 rounded-lg text-white ">Buy Now</button>
+                <button className=" px-4 py-2 text-xl bg-base-200 rounded-lg text-white"><span className=""><FcLike/></span></button>
+                <button className=" px-4 py-2 text-xl bg-base-200 text-red-400 rounded-lg"><span className=""><GrView/></span></button>
                 </div>
               </div>
+              
             </div>
+          
           </div>
         </div>
       </div>
