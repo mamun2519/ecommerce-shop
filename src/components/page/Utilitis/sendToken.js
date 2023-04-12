00ac37305c3a542a5b8e8162f903d634549c18ca
Md Mamun <juborajvai22@gmail.com>
2023-04-12 16:30:03 +0600

    added data

--- src/components/page/Utilitis/sendToken.js
+++ src/components/page/Utilitis/sendToken.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const sendToken = async(myForm) => {
-  
     await axios({
       method: "post",
       url: "https://ecommerce-shop-server.vercel.app/user/create",
@@ -9,6 +8,7 @@ const sendToken = async(myForm) => {
       headers: { "Content-Type": "multipart/form-data" },
     })
       .then((res) => {
+        console.log(res)
         localStorage.setItem("UserToken", res.data.token);
         localStorage.setItem("UserId", res.data.user._id);
       })
