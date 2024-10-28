28f173e92d67dd1fbb0e3883ada9887ae25fa426
mamun2519 <juborajislam46@gmail.com>
2024-10-28 23:26:04 +0600

    complete bug solve

--- src/components/page/Dashboard/UpdateProfile.js
+++ src/components/page/Dashboard/UpdateProfile.js
@@ -70,11 +70,7 @@ const UpdateProfile = () => {
 
     await axios({
       method: "put",
-<<<<<<< HEAD
       url: `http://localhost:5000/user/update/${userId}`,
-=======
-      url: `http://207.244.230.118:5000/user/update/${userId}`,
->>>>>>> 9e080e4f0e88e48ea1ed5c2ec24c25c8a6de7c7e
       data: myForm,
       headers: { "Content-Type": "multipart/form-data" },
     })
